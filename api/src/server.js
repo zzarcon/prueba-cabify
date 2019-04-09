ac300b016481c0edb0b2909f0bd24c221a780b49
vilvadot <hi@vilva.io>
2019-04-09 13:44:29 +0100

    Abstracted database connection and included it in tests

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ import database from './database'
 
 const {PORT} = process.env
 
-// database.connect()
+database.connect()
 
 app.listen(PORT, () => {
   console.log(`Listening on: localhost:${PORT}!!`)
--- api/test/controllers/checkout.test.js
+++ api/test/controllers/checkout.test.js
@@ -1,15 +1,20 @@
-import request from 'supertest'
-import app from "../../src/app"
+import request from "supertest";
+import app from "../../src/app";
+import database from "../../src/database";
 
-describe('Test the root path', () => {
-  test('It should response the GET method', done => {
-			console.log(process.env.DATABASE_URI)
-      return request(app)
+describe("Test the root path", () => {
+  afterAll(() => {
+    database.disconnect()
+  });
+
+  test("It should response the GET method", done => {
+    console.log(process.env.DATABASE_URI);
+    return request(app)
       .get("/api/checkout/123")
       .then(response => {
-					console.log(response.body)
-          expect(response.statusCode).toBe(200)
-          done()
-      })
+        console.log(response.body);
+        expect(response.statusCode).toBe(200);
+        done();
+      });
   });
-})
+});
