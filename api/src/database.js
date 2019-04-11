59bb41b1634e754faec3d6f746c92400dd6c4f8d
vilvadot <hi@vilva.io>
2019-04-11 16:43:38 +0100

    Refactored database to class

--- api/src/database.js
+++ api/src/database.js
@@ -1,26 +1,23 @@
 import mongoose from "mongoose";
 
-// TODO: Better to have a Class here or functions?
 const { DATABASE_URI } = process.env;
 
-const connect = () => {
-  mongoose.connect(DATABASE_URI, { useNewUrlParser: true });
-  mongoose.connection.on("error", err => {
-    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-  });
-  mongoose.Promise = global.Promise;
-};
+class Database {
+	static connect(){
+		mongoose.connect(DATABASE_URI, { useNewUrlParser: true });
+		mongoose.connection.on("error", err => {
+			console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+		});
+		mongoose.Promise = global.Promise;
+	};
 
-const disconnect = () => {
-  return mongoose.connection.close();
-};
+	static disconnect(){
+		return mongoose.connection.close();
+	};
 
-const drop = () => {
-	return mongoose.connection.db.dropDatabase()
+	static drop(){
+		return mongoose.connection.db.dropDatabase()
+	}
 }
 
-export default {
-  connect,
-	disconnect,
-	drop
-};
+export default Database
