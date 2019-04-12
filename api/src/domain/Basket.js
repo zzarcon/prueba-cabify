11da2fc3334c86adb00664e5f0ca070fa44e900e
vilvadot <hi@vilva.io>
2019-04-12 13:08:38 +0100

    Id checking in Basket

--- api/src/domain/Basket.js
+++ api/src/domain/Basket.js
@@ -1,13 +1,23 @@
 class Basket {
-	constructor(id, products){
+	constructor(id, products = []){
+		if(!id){
+			throw new Error('Cannot create a Basket without an ID')
+		}
+
 		this.id = id
 		this.products = products
+		this.amount = this._calculateAmount()
+	}
+
+	_calculateAmount(){
+		return 0
 	}
 
 	serialize(){
 		return {
 			id: this.id,
-			products: this.products
+			products: this.products,
+			amount: 0
 		}
 	}
 }
--- api/test/domain/Basket.test.js
+++ api/test/domain/Basket.test.js
@@ -2,16 +2,34 @@ import Basket from '../../src/domain/Basket'
 import "@babel/polyfill";
 
 describe("Basket", () => {
-  test("it can be serialized", async () => {
+
+	test('needs an ID to be created', () => {
+		const errorMessage = 'Cannot create a Basket without an ID'
+		expect(() => {
+			const aBasket = new Basket()
+		}).toThrowError(errorMessage)
+
+	})
+
+	test("by default has no products", () => {
 		const id = '1337'
-		const products = ['a','b']
+
+		const aBasket = new Basket(id)
+		const products = aBasket.products
+
+		expect(products.length).toEqual(0)
+	})
+  test("it can be serialized", () => {
+		const id = '1337'
+		const products = []
 
 		const epectedObject = {
 			id: '1337',
-			products: ['a','b']
+			products: [],
+			amount: 0
 		}
 
 		const aBasket = new Basket(id, products)
 		expect(aBasket.serialize()).toMatchObject(epectedObject)
-  });
+	});
 });
