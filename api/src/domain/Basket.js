5829a9b252fa0055940175dd297f250a4c5ab280
vilvadot <hi@vilva.io>
2019-04-12 13:55:48 +0100

    Basket knows how to handle 2x1 products

--- api/src/domain/Basket.js
+++ api/src/domain/Basket.js
@@ -1,3 +1,8 @@
+const PROMO_CODES = {
+	twoForOne: '2X1',
+	bulk : 'BULK'
+}
+
 class Basket {
 	constructor(id, products = []){
 		if(!id){
@@ -5,15 +10,31 @@ class Basket {
 		}
 		this.id = id
 		this.products = products
-		this.amount = this._calculateAmount()
+		this.amount = 0
+		this._calculateAmount()
 	}
 
 	_calculateAmount(){
-		return 0
+		let totalAmount = 0
+		let twoForOneCounter = 0
+		// TODO: Refactor
+		this.products.map((product, index) => {
+			let productPrice = product.price
+			if(product.promotion === PROMO_CODES.twoForOne){
+				twoForOneCounter++
+			}
+			if(twoForOneCounter === 1){
+				twoForOneCounter = 0
+				productPrice = product.price/2
+			}
+			totalAmount = totalAmount + productPrice
+		})
+		this.amount = totalAmount
 	}
 
 	addProduct(product){
 		this.products.push(product)
+		this._calculateAmount()
 	}
 
 	serialize(){
--- api/test/domain/Basket.test.js
+++ api/test/domain/Basket.test.js
@@ -58,4 +58,33 @@ describe("Basket", () => {
     expect(firstProductInBasket).toEqual(aProduct);
     expect(lastProductInBasket).toEqual(anotherProduct);
 	});
+
+	test('handles simple products price', () => {
+		const aProduct = new Product("LSBR", "lightsaber", 100);
+		const basket = new Basket("1337");
+		const combinedProductAmount = 300
+
+		basket.addProduct(aProduct)
+		basket.addProduct(aProduct)
+		basket.addProduct(aProduct)
+
+		expect(basket.amount).toBe(combinedProductAmount)
+	})
+
+	test('handles 2x1 promotions', () => {
+		const promotion = '2X1'
+
+		const aProduct = new Product("LSBR", "lightsaber", 100, promotion);
+		const basket = new Basket("1337");
+		const productAmountApplying2x1 = 200
+
+		basket.addProduct(aProduct)
+		basket.addProduct(aProduct)
+		basket.addProduct(aProduct)
+		basket.addProduct(aProduct)
+
+		expect(basket.amount).toBe(productAmountApplying2x1)
+	})
+
+	// Handle 2x1 incomplete orders
 });
